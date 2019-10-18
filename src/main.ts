33d0ef10e4099416aa591f119e05be9711315fff
Jeff <jeff@jeff.local>
2019-10-18 10:12:24 -0400

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { getMessage } from "./inner-lib";
+import { callGetMessage } from "./outer-lib";
+
+const msgFromInnerLib: string = getMessage();
+console.log(`msgFromInnerLib: ${msgFromInnerLib}`);
+
+const msgFromOuterLib = callGetMessage();
+console.log(`msgFromOuterLib: ${msgFromOuterLib}`);
